d1923d3d5a046855d6af56cc938ca27b3009b9e6
Preethamsu <preetham.umesh2003@gmail.com>
2026-02-11 22:55:55 +0100

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 def aqi_category(aqi):
-    if aqi <= 50: return "Good 😊"
-    elif aqi <= 100: return "Moderate 😐"
-    elif aqi <= 150: return "Unhealthy for Sensitive 😷"
-    elif aqi <= 200: return "Unhealthy 🚨"
-    elif aqi <= 300: return "Very Unhealthy ☠️"
-    else: return "Hazardous 🔴"
+    if aqi <= 50: return "Good"
+    elif aqi <= 100: return "Moderate"
+    elif aqi <= 150: return "Unhealthy for Sensitive"
+    elif aqi <= 200: return "Unhealthy"
+    elif aqi <= 300: return "Very Unhealthy"
+    else: return "Hazardous"
